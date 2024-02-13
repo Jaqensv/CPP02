cc3305f599e18442fc6802ed6ff59ac63d0d4bd9
Matthieu De Langhe <mde-lang@k0r4p12.42mulhouse.fr>
2024-02-13 19:06:49 +0100

    ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mde-lang <mde-lang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 17:55:45 by mde-lang          #+#    #+#             */
-/*   Updated: 2024/02/13 18:58:46 by mde-lang         ###   ########.fr       */
+/*   Updated: 2024/02/13 19:06:02 by mde-lang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main()
     Fixed const c( 42.42f );
     Fixed const d( b );
     
-    a = Fixed( 1234.4321f ); // pourquoi suis-je sense appeler deux fois mon operateur d'assignation ???
+    a = Fixed( 1234.4321f );
     
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
